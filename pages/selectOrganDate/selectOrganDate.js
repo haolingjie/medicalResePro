002897307f60b0e92d764f222aa657127e308ff9
haolingjie <haoljsh@sinosoft.com.cn>
2019-07-14 01:51:26 +0800

    代码提交

--- pages/selectOrganDate/selectOrganDate.js
+++ pages/selectOrganDate/selectOrganDate.js
@@ -169,7 +169,7 @@ Page({
         if (res.data.code == 0){
           wx.showModal({
             title: '提示',
-            content: '请耐心等待，系统将在 24H内以微信公众号提示形式发送给指定手机，系统可以随时登陆查看，系统可随时登陆查看',
+            content: '请耐心等待，系统将在24H内以微信公众号提示形式发送给指定手机，系统可以随时登陆查看',
             success: function (res) {
               if (res.confirm) {
                 wx.navigateTo({
